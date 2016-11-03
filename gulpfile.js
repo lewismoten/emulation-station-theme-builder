4da7526cc482dd82b4581fd6129e4136d4ecc863
Lewis Moten <lewismoten@gmail.com>
2016-11-02 23:50:48 -0400

    setup demo dragon drops

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+let gulp = require('gulp'),
+  browserSync = require('browser-sync')
+  .create();
+
+gulp.task('browser-sync', () => {
+
+  'use strict';
+
+  browserSync.init({
+    server:
+    {
+      baseDir: './'
+    }
+  });
+
+});
